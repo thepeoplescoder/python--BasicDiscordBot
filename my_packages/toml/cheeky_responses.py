41b3ce12d75e6b83d2c073cc635abc305feac25f
thepeoplescoder <thepeoplescoder@gmail.com>
2021-11-18 01:50:46 -0800

    Fixed AttributeError.

--- my_packages/toml/cheeky_responses.py
+++ my_packages/toml/cheeky_responses.py
@@ -5,7 +5,6 @@ import toml as _toml
 # Load the configuration file.
 try:
     toml = Path(__file__).parent / "tomls" / "cheeky_responses.toml"
-    toml = str(toml)
     toml = _toml.load(toml)
 
 # In this case, it's a fatal error if we can't find the .toml file.
@@ -13,7 +12,7 @@ except FileNotFoundError as ex:
     import sys
     from ..common import terminal as t
     print(end=t.bright_red("Fatal error: "))
-    print(t.bright_yellow(f"{toml} not found."))
+    print(t.bright_yellow(f"{str(toml)} not found."))
     print()
     toml = toml.name
     print(t.bright_yellow(f"Did you forget to copy _{toml} to {toml}?"))
